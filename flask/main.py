8bcab7f4a3e81fbbd1d80253264e65c044df7d01
Thiago Ribeiro <thiago.bernardes@aluno.ifsp.edu.br>
2023-03-11 23:10:31 -0300

    Publicação da Valaciação Contínua 13

--- flask/main.py
+++ flask/main.py
@@ -1,5 +1,6 @@
 import logging
 import traceback
+from flask import redirect
 
 import flask
 from replit import db
@@ -10,23 +11,19 @@ app = flask.Flask(__name__)
 def internal_server_error(e: str):
     return flask.jsonify(error=str(e)), 500
 
-
 @app.route('/', methods=['GET', 'POST'])
 def cadastroContatos():
   try:
     contatos = db.get('contatos', {}); 
     print(contatos);
     if flask.request.method == "POST":      
-      contatos[flask.request.form['email']] = {'nome': flask.request.form['nome'],
-                                              'telefone': flask.request.form['telefone'],
-                                              'assunto': flask.request.form['assunto'],
-                                              'mensagem': flask.request.form['mensagem'],
-                                              'resposta': flask.request.form['resposta']}
+      contatos[flask.request.form['email']] = {'nome': flask.request.form['nome'], 'telefone': flask.request.form['telefone'],'assunto': flask.request.form['assunto'], 'mensagem': flask.request.form['mensagem'],'re': flask.request.form['re']}
     db['contatos'] = contatos
     return flask.render_template('contatos.html', contatos=contatos)
   except Exception as e:
     logging.exception('failed to database')
     flask.abort(500, description=str(e) + ': ' + traceback.format_exc())
+
 
 @app.route('/limparBanco', methods=['POST'])
 def limparBanco():
@@ -36,5 +33,17 @@ def limparBanco():
   except Exception as e:
     logging.exception(e)
     return flask.render_template('contatos.html')
+
+
+@app.route('/deletarContato/<email>', methods=['POST'])
+def deletarContato(email):
+    try:
+      contatos = db.get('contatos', {});
+      del contatos[email];
+      db['contatos'] = contatos;
+      return redirect('/');
+    except Exception as e:
+      logging.exception(e);
+      return flask.render_template('contatos.html');
     
 app.run('0.0.0.0')
